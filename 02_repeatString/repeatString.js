d03f628e8e723e8ff68d1fe94645b8c37aef6463
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-10-27 13:13:03 +1100

    Please enter the commit message for your changes. Lines

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+//let userWordInput = prompt("Give me a word");
+//let userNumberInput = parseInt(prompt("Give me a number"));
 
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let result = ''; // Initialize an empty string to store the concatenated result
+    for (let i = 0; i < num; i++) {
+        result += string; // Concatenate the input string with the result string
+    }
+    return result; // Return the concatenated result
 };
+
+let repeatedString = repeatString('hey', 3);
+console.log(repeatedString); // Output: heyheyhey
+
 
 // Do not edit below this line
 module.exports = repeatString;
