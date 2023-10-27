a4846bbcb01ae467a823e6955782a80925bfdfce
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-10-27 15:31:19 +1100

    exercice ready for test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+let sentence = "hello there";
+let wordsArray = sentence.split(" ");
+console.log(wordsArray);
 
+let reversedArray = wordsArray.reverse();
+console.log(reversedArray);
+
+const reverseString = function(wordsArray) {
+  let result = "";
+  for (let i = 0; i < wordsArray.length; i++) {
+    result = wordsArray[i] + " " + result;
+  }
+  return result;
 };
+
+
+let reversedString = reverseString(wordsArray).slice(0, -1);
+console.log(reversedString); 
 
 // Do not edit below this line
 module.exports = reverseString;
