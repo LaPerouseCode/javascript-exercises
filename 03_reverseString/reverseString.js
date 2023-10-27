c936d4180ecb24f7ca62afbc53da0b1095191e60
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-10-27 16:25:42 +1100

    last edit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-let sentence = "hello";
-let wordsArray = sentence.split(" ");
+let sentence = "hello there is this working";
+let wordsArray = sentence.split("");
 console.log(wordsArray);
 
 let reversedArray = wordsArray.reverse();
@@ -8,13 +8,14 @@ console.log(reversedArray);
 const reverseString = function(wordsArray) {
   let result = "";
   for (let i = 0; i < wordsArray.length; i++) {
-    result = result + wordsArray[i] + " ";
+    result = result + wordsArray[i] + "";
   }
   return result.trim(); // Use trim to remove the trailing space at the end
 };
 
 let reversedString = reverseString(reversedArray);
 console.log(reversedString); // Output: "there hello"
+
 
 
 // Do not edit below this line
