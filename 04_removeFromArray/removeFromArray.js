2aca0e5a619af78a56d6398d08852bc5c7d7f028
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-10-28 14:31:08 +1100

    I used function filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,17 @@
 let myArray
 let itemOut 
 let itemOut2
+let itemOut3
+let itemOut4
+let itemOut5
+let itemOut6
+let itemOut7
+let itemOut8
 const removeFromArray = function(myArray, itemOut, itemOut2) {
     if (Array.isArray(itemOut)) {
         return myArray.filter(item => !itemOut.includes(item));
     } else {
-        return myArray.filter(item => item !== itemOut && item !== itemOut2);
+        return myArray.filter(item => item !== itemOut && item !== itemOut2 && item !== itemOut3 && item !== itemOut4 && item !== itemOut5 && item !== itemOut6 && item !== itemOut7 && item !== itemOut8);
     }
 };
 
