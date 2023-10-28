0397870072a09984f452c1426c2d56d1f5fe5f3c
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-10-28 17:03:58 +1100

    finished - testing now

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(chosenYear) {
+    let n = chosenYear;
 
+    if ((n % 4 === 0 && n % 100 !== 0) || (n % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
+let V = leapYears(1985);
+console.log(V);
 
 // Do not edit below this line
 module.exports = leapYears;
