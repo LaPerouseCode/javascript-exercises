f2d74a8442ebe12af0d44f739744bdd127b83dd9
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-10-28 14:27:39 +1100

    I used function filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,16 @@
 let myArray
 let itemOut 
-const removeFromArray = function(myArray, itemOut) {
-if (Array.isArray(itemOut)) {
-return myArray.filter(item => !itemOut.includes(item));
-} else { return myArray.filter(item => item !==itemOut);}
-}
-let result = removeFromArray([1, 2, "john", 4, 5], [2, 3, "john"])
-console.log(result);
+let itemOut2
+const removeFromArray = function(myArray, itemOut, itemOut2) {
+    if (Array.isArray(itemOut)) {
+        return myArray.filter(item => !itemOut.includes(item));
+    } else {
+        return myArray.filter(item => item !== itemOut && item !== itemOut2);
+    }
+};
 
+let result = removeFromArray([1, 2, "john", 4, 5], [2, 3, "john"], "tacos");
+console.log(result);
 
 // Do not edit below this line
 module.exports = removeFromArray;
