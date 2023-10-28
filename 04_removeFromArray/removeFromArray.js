03176a368d6010c29670699089ac795f368e4074
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-10-28 14:19:56 +1100

    I used function filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,12 @@
 let myArray
 let itemOut 
-
 const removeFromArray = function(myArray, itemOut) {
+if (Array.isArray(itemOut)) {
 return myArray.filter(item => !itemOut.includes(item));
-
-};
-
-let result = removeFromArray([1, 2, "john", 4, 5], "john")
+} else { return myArray.filter(item => item !==itemOut);}
+}
+let result = removeFromArray([1, 2, "john", 4, 5], [2, 3, "john"])
 console.log(result);
-
-
 
 
 // Do not edit below this line
