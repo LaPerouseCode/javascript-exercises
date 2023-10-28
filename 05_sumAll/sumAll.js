23d3bf4d2e44905db0848d6cb0636f86445560bd
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-10-28 16:40:14 +1100

    I used function filter

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,8 +2,10 @@ function sumAll(fromNumber, toNumber) {
     if (fromNumber < 0 || toNumber < 0) {
         console.log('ERROR');
         return 'ERROR';
+    } else if (typeof fromNumber !== 'number' || typeof toNumber !== 'number') {
+            console.log('ERROR');
+            return 'ERROR';
     }
-
     let i = Math.min(fromNumber, toNumber);
     let end = Math.max(fromNumber, toNumber);
     let addition = 0;
