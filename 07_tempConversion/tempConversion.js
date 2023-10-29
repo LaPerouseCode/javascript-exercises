23d2a65cc216e7e540a49ebf5cba8500da94a9c8
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-10-29 12:11:42 +1100

    finished - testing now

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+let inputFahrenheit;
+const convertToCelsius = function(inputFahrenheit) {
+return Math.round((inputFahrenheit - 32)*(5/9)*10)/10;
 };
+let C = convertToCelsius(100);
+console.log(C);
 
-const convertToFahrenheit = function() {
+let inputCelsius;
+const convertToFahrenheit = function(inputCelsius) {
+return Math.round((inputCelsius * 9/5 + 32)*10)/10;
 };
+let F = convertToFahrenheit(35.5887777778);
+console.log(F);
+
+
 
 // Do not edit below this line
 module.exports = {
