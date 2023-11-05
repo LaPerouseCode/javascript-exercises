8d1f26a3f3b1c3286be187257f053fa419f0e97d
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-11-05 14:12:07 +1100

    Finished ex8 JS

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+let addition = 0;
+const add = function(a, b) {
+  addition += a + b;
 };
 
-const subtract = function() {
-	
+let sub = 0;
+const subtract = function(a, b) {
+  sub += a - b;
 };
 
+let sumMyArray = [1, 4, 7]
+let totalSum = 0;
 const sum = function() {
-	
+  for (let i = 0; i < sumMyArray.length; i++) {
+  totalSum += sumMyArray[i];
+  }
 };
 
+let totalMul = sumMyArray[0];
 const multiply = function() {
-
+  for (let i = 0; i < sumMyArray.length; i++) {
+    totalMul *= sumMyArray[i];
+  }
 };
 
-const power = function() {
-	
-};
+const power(base, exponent) {
+  return Math.pow(base, exponent);
+}
 
-const factorial = function() {
-	
+let totalFact = 1;
+const factorial = function(n) {
+  for (let i = 1; i <= n; i++) {
+    totalFact *= i;
+  }
+  return totalFact;
 };
 
 // Do not edit below this line
